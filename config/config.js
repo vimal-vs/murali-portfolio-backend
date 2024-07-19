10b66a0373f5e7d448b2bad3909739f49ec6b82e
Vimal Vs <vimalchan24@gmail.com>
2024-07-19 13:11:53 +0530

    prod config setup

--- config/config.js
+++ config/config.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const { parse } = require('pg-connection-string');
+
+const supabaseConfig = parse(process.env.SUPABASE_CONNECTION_URL);
 
 module.exports = {
     development: {
@@ -9,10 +12,11 @@ module.exports = {
         dialect: 'postgres'
     },
     production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        username: supabaseConfig.user,
+        password: supabaseConfig.password,
+        database: supabaseConfig.database,
+        host: supabaseConfig.host,
+        port: supabaseConfig.port,
         dialect: 'postgres'
     }
 };
