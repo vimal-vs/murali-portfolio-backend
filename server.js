46282fe475c62d5b17fe842a73409597b01441a7
Vimal Vs <vimalchan24@gmail.com>
2024-07-19 13:37:53 +0530

    cors origin added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://localhost:3002'],
     credentials: true,
 }));
 
